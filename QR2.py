0e801aebb7d702b675bfa259b6699ab2000ed1c9
Shri_shinigami <shriyesh.chandra@gmail.com>
2020-11-13 16:21:58 +0530

    Final Noiseless QR Code reader
    Here I have tested 3 filters which gave positive results and concluded with median filter as the best one.

--- QR2.py
+++ QR2.py
@@ -4,13 +4,16 @@ import numpy
 def scan():
     i=0
     cap=cv2.VideoCapture(0)
-    while i<4:
+        while i<1:
         _,frame=cap.read()
-        decoded=pyzbar.decode(frame)
+        #fil = cv2.bilateralFilter(frame, 9, 75, 75)
+        #fil= cv2.fastNlMeansDenoisingColored(frame,None, 10, 10, 7, 15)
+        fil=cv2.medianBlur(frame, 3)
+        decoded=pyzbar.decode(fil)
         for obj in decoded:
             print(obj.data)
             i=i+1
-        cv2.imshow("QRCODE",frame)
+        cv2.imshow("QRCODE",fil)
         cv2.waitKey(5)
         cv2.destroyAllWindows()
 scan()
